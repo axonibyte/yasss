983c06a99b41c0e4f30788fbbe6ed03da5c337f1
Caleb L. Power <cpower@axonibyte.com>
2024-10-01 01:05:55 -0400

    implement UI follow-thru on activity modal... implement better workflows for custom modal button functionality, data persistence, etc.; fix a few bugs, bad identifiers...; make modal titles dynamic (for post and patch workflows); make sure on click event handlers aren't duplicated; make sure activity crud (for the frontend table) doesn't bork the table

--- src/main/resources/public/assets/js/app.js
+++ src/main/resources/public/assets/js/app.js
@@ -3,6 +3,8 @@ var eventTableData = {
   "rows": []
 };
 
+var eventChanges = {};
+
 function addCell(parent, label, aesthetics = 'is-outlined is-primary', fn = null, data = {}) {
   let cell = $('<div/>')
     .addClass('cell')
@@ -87,7 +89,9 @@ var viewTableSliderOutput = $('<output/>')
   .attr('for', 'view-event-slider')
   .hide();
 
-function renderTableSlider(parent, step, max) {
+function renderTableSlider(parent, step = 1, max = null) {
+  if(null == max)
+    max = eventTableData.headers.length > 4 ? 5 : (eventTableData.headers.length + 1);
   parent.children('input.slider').remove();
   parent
     .append(
@@ -105,12 +109,14 @@ function renderTableSlider(parent, step, max) {
   bulmaSlider.attach();
 }
 
-function renderEventSummaryModal(newEvent = true, fn = null, summary = {
+function renderEventSummaryModal(newEvent = true, savFn = null, summary = {
   title: '',
   description: '',
   notifyOnSignup: true,
   allowMultiuserSignups: false
 }) {
+  $('#edit-event-submit').unbind('click');
+
   $('#edit-event-modal p.modal-card-title').text(
       newEvent ? 'Create an Event' : 'Update an Event');
 
@@ -119,9 +125,9 @@ function renderEventSummaryModal(newEvent = true, fn = null, summary = {
   $('#edit-event-notify-switch').prop('checked', summary.notifyOnSignup);
   $('#edit-event-multiuser-switch').prop('checked', summary.allowMultiuserSignups);
 
-  if('function' === typeof fn) {
+  if('function' === typeof savFn) {
     $('#edit-event-submit').on('click', function() {
-      if(fn(summary))
+      if(savFn(summary))
         $('#edit-event-modal').removeClass('is-active');
     });
   }
@@ -129,52 +135,70 @@ function renderEventSummaryModal(newEvent = true, fn = null, summary = {
   $('#edit-event-modal').addClass('is-active');  
 }
 
-function renderEventActivityModal(activity = {
+function renderEventActivityModal(newActivity = true, savFn = null, delFn = null, activity = {
   label: '',
   description: '',
   activityVolunteerCap: -1,
   slotVolunteerCapDefault: -1
-}, savFn = null, delFn = null) {
-  $('#edit-activity-short-descr').attr('value', activity.label);
-  $('#edit-activity-long-descr').attr('value', activity.description);
-  if(0 <= activity.activityVolunteerCap) {
+}) {
+  console.log(activity);
+
+  $('#edit-activity-sav').unbind('click');
+  $('#edit-activity-del').unbind('click');
+
+  $('#edit-activity-modal p.modal-card-title').text(
+      newActivity ? 'Add an Activity' : 'Update an Activity');
+
+  $('#edit-activity-short-descr').val(activity.label);
+  $('#edit-activity-long-descr').val(activity.description);
+  if(-1 == activity.activityVolunteerCap) {
     $('#edit-activity-vol-cap-switch').prop('checked', true);
-    $('#edit-activity-vol-cap-field').attr('value', '').hide();
+    $('#edit-activity-vol-cap-field').val('');
+    $('.toggle-activity-vol-cap').not('.toggle').hide();
   } else {
     $('#edit-activity-vol-cap-switch').prop('checked', false);
-    $('#edit-activity-vol-cap-field').attr('value', activity.activityVolunteerCap).show();
+    $('#edit-activity-vol-cap-field').val(activity.activityVolunteerCap);
+    $('.toggle-activity-vol-cap').not('.toggle').show();
   }
-  if(0 <= activity.slotVolunteerCapDefault) {
-    $('#edit-slot-val-cap-def-switch').prop('checked', true);
-    $('#edit-slot-val-cap-def-field').attr('value', '').hide();
+  if(-1 == activity.slotVolunteerCapDefault) {
+    $('#edit-activity-slot-vol-cap-def-switch').prop('checked', true);
+    $('#edit-activity-slot-vol-cap-def-field').val('');
+    $('.toggle-slot-vol-def-cap').not('.toggle').hide();
   } else {
-    $('#edit-slot-val-cap-def-switch').prop('checked', false);
-    $('#edit-slot-val-cap-def-field').attr('value', activity.slotVolunteerCapDefault).show();
+    $('#edit-activity-slot-vol-cap-def-switch').prop('checked', false);
+    $('#edit-activity-slot-vol-cap-def-field').val(activity.slotVolunteerCapDefault);
+    $('.toggle-slot-vol-def-cap').not('.toggle').show();
   }
 
-  if('function' === typeof savFn) {
+  if('function' === typeof savFn)
     $('#edit-activity-sav').on('click', function() {
       if(savFn(activity))
         $('#edit-activity-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-activity-sav').hide();
 
-  if('function' === typeof delFn) {
+  if('function' === typeof delFn)
     $('#edit-activity-del').on('click', function() {
       if(delFn(activity))
         $('#edit-activity-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-activity-del').hide();
 
   $('#edit-activity-modal').addClass('is-active');
 }
 
-function renderEventWindowModal(window = {
+function renderEventWindowModal(newWindow = true, savFn = null, delFn = null, window = {
   startDate: '',
   startTime: '',
   endDate: '',
   endTime: ''
-}, savFn = null, delFn = null) {
+}) {
+  $('#edit-window-sav').unbind('click');
+  $('#edit-window-del').unbind('click');
+
+  $('#edit-window-modal p.modal-card-title').text(
+      newWindow ? 'Add a Window' : 'Update a Window');
+
   let cal = $('#edit-window-range')[0].bulmaCalendar;
   if('' == startDate || '' == startTime || '' == endDate || '' == endTime)
     cal.clear();
@@ -186,63 +210,74 @@ function renderEventWindowModal(window = {
     cal.save();
   }
 
-  if('function' === typeof savFn) {
+  if('function' === typeof savFn)
     $('#edit-window-sav').on('click', function() {
       if(savFn(window))
         $('#edit-window-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-window-sav').hide();
 
-  if('function' === typeof delFn) {
+  if('function' === typeof delFn)
     $('#edit-window-del').on('click', function() {
       if(delFn(window))
         $('#edit-window-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-window-del').hide();
 
   $('#edit-window-modal').addClass('is-active');
 }
 
-function renderEventDetailModal(detail = {
+function renderEventDetailModal(newDetail = true, savFn = null, delFn = null, detail = {
   'type': '',
   'field': '',
   'description': '',
   'required': false
-}, saveFn = null, delFn = null) {
+}) {
+  $('#edit-detail-sav').unbind('click');
+  $('#edit-detail-del').unbind('click');
+
+  $('#edit-detail-modal p.modal-card-title').text(
+      newDetail ? 'Add a Detail' : 'Update a Detail');
+
   if('' == detail.type)
     $('#edit-detail-type-dropdown option:contains("?")').prop('selected', true);
   else $(`#edit-detail-type-dropdown option[value="${detail.type}"]`).prop('selected', true);
 
-  $('#edit-detail-field').attr('value', detail.field);
-  $('#edit-detail-descr').text(detail.field);
+  $('#edit-detail-field').val(detail.field);
+  $('#edit-detail-descr').val(detail.field);
   $('#edit-detail-required-switch').prop('checked', detail.required);
 
-  if('function' === typeof savFn) {
+  if('function' === typeof savFn)
     $('#edit-detail-sav').on('click', function() {
       if(savFn(detail))
         $('#edit-detail-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-detail-sav').hide();
 
-  if('function' === typeof delFn) {
+  if('function' === typeof delFn)
     $('#edit-detail-del').on('click', function() {
       if(delFn(detail))
         $('#edit-detail-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-detail-del').hide();
 
   $('#edit-detail-modal').addClass('is-active');
 }
 
-function renderEventSlotModal(slot = {
+function renderEventSlotModal(newSlot = true, savFn = null, slot = {
   'activitySummary': '',
   'activityEditFn': null,
   'windowSummary': '',
   'windowEditFn': null,
   'slotEnabled': true,
   'slotVolunteerCap': -1
-}, savFn = null) {
-  $('#edit-slot-activity-field').attr('value', slot.activitySummary);
+}) {
+  $('#edit-slot-sav').unbind('click');
+
+  $('#edit-slot-modal p.modal-card-title').text(
+      newEvent ? 'Add a Slot' : 'Update a Slot');
+
+  $('#edit-slot-activity-field').val(slot.activitySummary);
 
   if('function' === typeof slot.activityEditFn) {
     $('#edit-slot-activity-btn').on('click', () => {
@@ -250,7 +285,7 @@ function renderEventSlotModal(slot = {
     }).show();
   } else $('#edit-slot-activity-btn').hide();
 
-  $('#edit-slot-window-field').attr('value', slot.windowSummary);
+  $('#edit-slot-window-field').val(slot.windowSummary);
 
   if('function' === typeof slot.windowEditFn) {
     $('#edit-slot-window-btn').on('click', () => {
@@ -266,22 +301,41 @@ function renderEventSlotModal(slot = {
     $('#edit-slot-cap-fields').hide();
   }
 
-  if(0 <= slot.slotVolunteerCap) {
+  if(-1 == slot.slotVolunteerCap) {
     $('#edit-slot-vol-cap-switch').prop('checked', true);
-    $('#edit-slot-vol-cap-field').attr('value', '').hide();
+    $('#edit-slot-vol-cap-field').val('');
+    $('.toggle-slot-vol-cap').not('.toggle').hide();
   } else {
     $('#edit-slot-vol-cap-switch').prop('checked', false);
-    $('#edit-slot-vol-cap-field').attr('value', slot.slotVolunteerCap).show();
+    $('#edit-slot-vol-cap-field').val(slot.slotVolunteerCap);
+    $('.toggle-slot-vol-cap').not('.toggle').show();
   }
 
-  if('function' === typeof savFn) {
+  if('function' === typeof savFn)
     $('#edit-slot-sav').on('click', function() {
       if(savFn(slot))
         $('#edit-slot-modal').removeClass('is-active');
-    });
-  }
+    }).show();
+  else $('#edit-slot-sav').hide();
 
   $('#edit-slot-modal').addClass('is-active');
+}
+
+function refreshTable() {
+  renderTable($('#view-event-table'));
+  renderTableSlider($('#view-event-table').parent());
+}
+
+function getActivityModalVals(newVals = null) {
+  let data = {
+    label: $('#edit-activity-short-descr').val(),
+    description: $('#edit-activity-long-descr').val(),
+    activityVolunteerCap: $('#edit-activity-vol-cap-switch').prop('checked')
+        ? -1 : $('#edit-activity-vol-cap-field').val(),
+    slotVolunteerCapDefault: $('#edit-activity-slot-vol-cap-def-switch').prop('checked')
+        ? -1 : $('#edit-activity-slot-vol-cap-def-field').val()
+  }
+  return null != newVals ? Object.assign(data, newVals) : data;
 }
 
 $(function() {
@@ -309,86 +363,93 @@ $(function() {
 
   viewTableSliderObserver.observe(viewTableSliderOutput[0], { childList: true, subtree: true, characterData: true });
 
-  eventTableData = { "headers": [], "rows": [] };
-  for(let header = 1; header <= 8; header++)
-    eventTableData.headers.push({
-      label: `Activity #${header}`,
-      fn: (data) => {
-        $('#edit-activity-modal').addClass('is-active');
-      }
-    });
-  for(let row = 0; row < 4; row++) {
-    eventTableData.rows.push({
-      label: `Window #${row + 1}`,
-      fn: (data) => {
-        $('#edit-window-modal').addClass('is-active');
-      }
-    });
-    for(let col = 0; col < 8; col++) {
-      eventTableData.rows.push({
-        label: `Slot #${row + 1}-${col + 1}`,
+  $('#magic-button').on('click', () => {
+    eventTableData = { "headers": [], "rows": [] };
+    for(let header = 1; header <= 8; header++)
+      eventTableData.headers.push({
+        label: `Activity #${header}`,
         fn: (data) => {
-          $('#edit-slot-modal').addClass('is-active');
+          $('#edit-activity-modal').addClass('is-active');
         }
       });
-    }
-  }
-
-  $('#magic-button').on('click', () => {
-    renderTable($('#view-event-table'));
-    renderTableSlider(
-        $('#view-event-table').parent(),
-        1,
-        eventTableData.headers.length > 4 ? 5 : (eventTableData.headers.length + 1),
-        (val) => {
-          console.log(`slider: ${val}`);
+    for(let row = 0; row < 4; row++) {
+      eventTableData.rows.push({
+        label: `Window #${row + 1}`,
+        fn: (data) => {
+          $('#edit-window-modal').addClass('is-active');
         }
-    );
+      });
+      for(let col = 0; col < 8; col++) {
+        eventTableData.rows.push({
+          label: `Slot #${row + 1}-${col + 1}`,
+          fn: (data) => {
+            $('#edit-slot-modal').addClass('is-active');
+          }
+        });
+      }
+    }
+
+    refreshTable();
   });
 
   // for when someone hits the 'create event' nav item
   $('#create-event-btn').on('click', () => {
+    eventTableData = {
+      "headers": [],
+      "rows": []
+    };
     renderEventSummaryModal(newEvent = true, fn = function(summary) {
       renderTableMeta(
         $('#edit-event-short-descr').val(),
         $('#edit-event-long-descr').val(),
         true);
-      eventTableData = {
-        "headers": [],
-        "rows": []
-      };
-      renderTable($('#view-event-table'));
-      renderTableSlider(
-          $('#view-event-table').parent(),
-          1,
-          eventTableData.headers.length > 4 ? 5 : (eventTableData.headers.length + 1),
-          (val) => {
-            console.log(`slider: ${val}`);
-          }
-      );
+      refreshTable();
       $('#view-event-section').show();
       return true;
     });
-  });
 
-  // for when someone wants to go back and edit the event summary
-  $('#view-event-edit-summary').on('click', () => {
-    $('#edit-event-modal').addClass('is-active');
-  });
+    // for when someone wants to go back and edit the event summary
+    $('#view-event-edit-summary').on('click', () => {
+      renderEventSummaryModal(newEvent = false); // fn null to keep prev. fn in place
+    });
 
-  // for when someone wants to add or modify event activities
-  $('#view-event-add-activity').on('click', () => {
-    $('#edit-activity-modal').addClass('is-active');
-  });
+    // for when someone wants to add or modify event activities
+    $('#view-event-add-activity').on('click', () => {
+      renderEventActivityModal(newActivity = true, savFn = function(activity) {
+        let data = getActivityModalVals({ idx: eventTableData.headers.length });
+        eventTableData.headers.push({
+          label: data.label,
+          fn: (d) => { // on click function
+            renderEventActivityModal(newActivity = false, savFn = function(activity) { // on save
+              Object.assign(activity, getActivityModalVals());
+              eventTableData.headers[data.idx].label = activity.label;
+              refreshTable();
+              return true;
+            }, delFn = function(activity) { // on delete
+              for(let i = d.idx + 1; i < eventTableData.headers.length; i++)
+                eventTableData.headers[i].data.idx--;
+              eventTableData.headers.splice(d.idx, 1);
+              refreshTable();
+              return true;
+            }, d); // last param is to set modal defaults
+          },
+          data: data // on save activity
+        });
 
-  // for when someone wants to add or modify event windows
-  $('#view-event-add-window').on('click', () => {
-    $('#edit-window-modal').addClass('is-active');
-  });
+        refreshTable();
+        return true;
+      }); // null activity is new activity
+    });
 
-  // for when someone wants to add or modify event details
-  $('#view-event-add-field').on('click', () => {
-    $('#edit-detail-modal').addClass('is-active');
+    // for when someone wants to add or modify event windows
+    $('#view-event-add-window').on('click', () => {
+      $('#edit-window-modal').addClass('is-active');
+    });
+
+    // for when someone wants to add or modify event details
+    $('#view-event-add-field').on('click', () => {
+      $('#edit-detail-modal').addClass('is-active');
+    });
   });
 
   // for when someone wants to log in or register
